c8ff867507291f396dea4090e224a0b5a8bc8334
Aldo Gomez <gomezfx@gmail.com>
2019-07-07 02:07:37 -0700

    added logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+import ThreeText from './components/ThreeText.js';
+import ThreeRotatingText from './components/ThreeRotatingText';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 30px;
+    bottom: 30px;
+    left: 30px;
+    right: 30px;
+    border: 2px solid #000;
+    z-index: 1;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  position: relative;
+  top: -128px;
+  z-index: 3;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <LogoWrapper>
+          <ThreeText height="320px" width="100vw">ALDOGOMEZ</ThreeText>
+        </LogoWrapper>
+      </Container>
     </div>
   );
 }
